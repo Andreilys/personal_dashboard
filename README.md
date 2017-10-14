7c6ed72f5c1c242cf08eb56458a695b76cdd3add
Andreilys <andreilyskov@gmail.com>
2017-10-13 20:00:25 -0400

    heroku push

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Personal Dashboard
+
+This is a personal dashboard I built to aggregate and visualize all the different tracking services that track my life. The dashboard is updated in real time and can be found at https://qself-dashboard.herokuapp.com/
+
+Right now it visualizes my:
+- Time spent online productivtily and unproductivity (Rescuetime)
+- Steps data (Moves)
+- Chess (lichess.com)
+- Weight (Withings smart scale)
+- Pomodoro time (Toggl.com)
+- Todo's (Todoist)
+- Time spent programming (Wakatime)
+- Music tastes (Spotify)
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Support](#support)
+- [Contributing](#contributing)
+
+## Installation
+
+
+```sh
+git clone https://github.com/Andreilys/personal_dashboard
+```
+
+## Usage
+
+## Support
+
+Please [open an issue](https://github.com/fraction/readme-boilerplate/issues/new) for support.
+
+## Contributing
+
+Please contribute using [Github Flow](https://guides.github.com/introduction/flow/). Create a branch, add commits, and [open a pull request](https://github.com/andreilys/personal_dashboard/compare).
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -67,7 +67,6 @@ function display_data(data) {
         $('#weather_today').html(data.weather_today);
         $('#current_steps').html("Steps today: " + String(data.current_steps)  + " steps");
         $('#average_past_seven_steps').html(data.average_past_seven_steps),
-        $('#coding').html('<embed id="coding_time" src="https://wakatime.com/share/@0c62f2ad-9fa5-43c7-a08f-7b1562918a7d/7979f8f3-d3f7-40d7-8dc1-0f18f054c272.svg"></embed> <embed id="coding_lang" src="https://wakatime.com/share/@0c62f2ad-9fa5-43c7-a08f-7b1562918a7d/b837f770-ca01-4fbe-ba68-7d1b03c76752.svg"></embed>');
         $('#chess_rating').html(data.chess_rating);
         $('#chess_games').html(data.chess_games);
         $('#daily_pomodoros').html(data.daily_pomodoros);
@@ -75,92 +74,228 @@ function display_data(data) {
         //remove loading text from HTML
         $('#loading').remove();
 
-        unproductivity_doughnut = get_unproductivity_doughnut()
-        steps_doughnut = get_steps_doughnut()
-        //This is meant to update the doughnut for unproductivity since my goal is to
-        // have less than 1 hour of unproductive time each day
-        var max_unproductivity_value = 1.5
-        // We need to get the difference between last update and current update.
-        // first we have to check if prev_Data is null so we don't throw an error
-        var difference_in_unproductivity = prev_data.hasOwnProperty(rescue_time_daily_unproductivity) ? data.rescue_time_daily_unproductivity - prev_data.rescue_time_daily_unproductivity : data.rescue_time_daily_unproductivity;
-        if ((unproductivity_doughnut.segments[0].value >= max_unproductivity_value) || (difference_in_unproductivity >= max_unproductivity_value)) {
-          unproductivity_doughnut.segments[0].value = max_unproductivity_value;
-          unproductivity_doughnut.segments[1].value = 0;
-          steps_doughnut.update()
-        } else {
-          unproductivity_doughnut.segments[0].value = unproductivity_doughnut.segments[0].value + difference_in_unproductivity;
-          unproductivity_doughnut.segments[1].value = unproductivity_doughnut.segments[1].value - difference_in_unproductivity;
-          unproductivity_doughnut.update();
-        }
+        $.ajax({
+            type: 'GET',
+            url: 'https://wakatime.com/share/@0c62f2ad-9fa5-43c7-a08f-7b1562918a7d/43cd4128-5361-43db-b51b-d965e3c575a5.json',
+            dataType: 'jsonp',
+            success: function(response) {
+              console.log(response.data);
+            },
+          });
 
-        //This is meant to update the steps doughnut since my goal is to have
-        // at least 5000 steps a day
-        var max_steps_value = 5000
-        // We need to get the difference between last update and current update.
-        // first we have to check if prev_Data is null so we don't throw an error
-        console.log(data.current_steps)
-        var difference_in_steps = prev_data.hasOwnProperty(current_steps) ? data.current_steps - prev_data.current_steps : data.current_steps;
-        if ((steps_doughnut.segments[0].value >= max_steps_value) || (difference_in_steps >= max_steps_value)) {
-          console.log('working')
-          steps_doughnut.segments[0].value = max_steps_value;
-          console.log(steps_doughnut.segments[0].value)
-          console.log(steps_doughnut)
-          steps_doughnut.segments[1].value = 0;
-          steps_doughnut.update()
-        } else {
-          console.log('workin?')
-          steps_doughnut.segments[0].value = steps_doughnut.segments[0].value + difference_in_steps
-          steps_doughnut.segments[1].value = steps_doughnut.segments[1].value - difference_in_steps
-          steps_doughnut.update()
-        }
+
+          $.ajax({
+            type: 'GET',
+            url: 'https://wakatime.com/share/@0c62f2ad-9fa5-43c7-a08f-7b1562918a7d/27967d19-0ce0-42a6-9f4a-c3c2440cf575.json',
+            dataType: 'jsonp',
+            success: function(response) {
+              console.log(response.data);
+            },
+          });
+
+        // unproductivity_doughnut = get_unproductivity_doughnut()
+        // steps_doughnut = get_steps_doughnut()
+        // //This is meant to update the doughnut for unproductivity since my goal is to
+        // // have less than 1 hour of unproductive time each day
+        // var max_unproductivity_value = 1
+        // // We need to get the difference between last update and current update.
+        // // first we have to check if prev_Data is null so we don't throw an error
+        // var difference_in_unproductivity = prev_data.hasOwnProperty(rescue_time_daily_unproductivity) ? data.rescue_time_daily_unproductivity - prev_data.rescue_time_daily_unproductivity : data.rescue_time_daily_unproductivity;
+        // if ((unproductivity_doughnut.segments[0].value >= max_unproductivity_value) || (difference_in_unproductivity >= max_unproductivity_value)) {
+        //   unproductivity_doughnut.segments[0].value = max_unproductivity_value;
+        //   unproductivity_doughnut.segments[1].value = 0;
+        //   steps_doughnut.update()
+        // } else {
+        //   unproductivity_doughnut.segments[0].value = unproductivity_doughnut.segments[0].value + difference_in_unproductivity;
+        //   unproductivity_doughnut.segments[1].value = unproductivity_doughnut.segments[1].value - difference_in_unproductivity;
+        //   unproductivity_doughnut.update();
+        // }
+        //
+        // //This is meant to update the steps doughnut since my goal is to have
+        // // at least 5000 steps a day
+        // var max_steps_value = 5000
+        // // We need to get the difference between last update and current update.
+        // // first we have to check if prev_Data is null so we don't throw an error
+        // var difference_in_steps = prev_data.hasOwnProperty(current_steps) ? data.current_steps - prev_data.current_steps : data.current_steps;
+        // if ((steps_doughnut.segments[0].value >= max_steps_value) || (difference_in_steps >= max_steps_value)) {
+        //   steps_doughnut.segments[0].value = max_steps_value;
+        //   steps_doughnut.segments[1].value = 0;
+        //   steps_doughnut.update()
+        // } else {
+        //   steps_doughnut.segments[0].value = steps_doughnut.segments[0].value + difference_in_steps
+        //   steps_doughnut.segments[1].value = steps_doughnut.segments[1].value - difference_in_steps
+        //   steps_doughnut.update()
+        // }
         // remember this data, in case want to compare it to next update
         prev_data = data;
       }
 }
 
-function get_unproductivity_doughnut(){
-    var max_value = 1.5;
-    chartOptions = {
-        segmentShowStroke: false,
-        percentageInnerCutout: 75,
-        animation: false
-    };
+// function get_unproductivity_doughnut(){
+//     var max_value = 1.5;
+//     chartOptions = {
+//         segmentShowStroke: false,
+//         percentageInnerCutout: 75,
+//         animation: false
+//     };
+//
+//     chartData = [{
+//         value: 0,
+//         color: '#ff0000'
+//     },{
+//         value: max_value,
+//         color: '#DDDDDD'
+//     }];
+//     var ctx = $('#unproductivity_doughnut').get(0).getContext("2d");
+//     var unproductivity_dougnut = new Chart(ctx).Doughnut(chartData,chartOptions);
+//     return unproductivity_dougnut
+// }
+//
+// function get_steps_doughnut(){
+//   var max_value = 5000
+//   chartOptions = {
+//     segmentShowStroke: false,
+//     percentageInnerCutout: 75,
+//     animation: false
+//   };
+//
+//   chartData = [{
+//       value: 0,
+//       color: '#4FD134'
+//   },{
+//       value: max_value,
+//       color: '#DDDDDD'
+//   }];
+//   var ctx = $('#steps_doughnut').get(0).getContext("2d");
+//   var steps_doughnut = new Chart(ctx).Doughnut(chartData,chartOptions);
+//   return steps_doughnut
+// }
 
-    chartData = [{
-        value: 0,
-        color: '#ff0000'
-    },{
-        value: max_value,
-        color: '#DDDDDD'
-    }];
-    var ctx = $('#unproductivity_doughnut').get(0).getContext("2d");
-    var unproductivity_dougnut = new Chart(ctx).Doughnut(chartData,chartOptions);
-    return unproductivity_dougnut
+function create_doughnuts(){
+  $(function () {
+    var totalRevenue = 15341110,
+        totalUsers = 7687036;
+
+    // CanvasJS doughnut chart to show annual sales percentage from United States(US)
+    var steps_doughnut = new CanvasJS.Chart("steps_doughnut", {
+      animationEnabled: true,
+      backgroundColor: "transparent",
+      title: {
+        fontColor: "#848484",
+        fontSize: 60,
+        horizontalAlign: "center",
+        text: "47%",
+        fontFamily: "Roboto",
+        verticalAlign: "center"
+      },
+      toolTip: {
+        backgroundColor: "#ffffff",
+        borderThickness: 0,
+        cornerRadius: 0,
+        fontColor: "#424242"
+      },
+      data: [
+        {
+          explodeOnClick: false,
+          innerRadius: "94%",
+          radius: "90%",
+          startAngle: 270,
+          type: "doughnut",
+          dataPoints: [
+            { y: 10, color: "#c70000", toolTipContent: "United States: $<span>" + CanvasJS.formatNumber(Math.round(47 / 100 * totalRevenue), '#,###,###') + "</span>" },
+            { y: 90, color: "#424242", toolTipContent: null }
+          ]
+        }
+      ]
+    });
+
+    // CanvasJS doughnut chart to show annual sales percentage from Netherlands(NL)
+    var pomodoro_doughnut = new CanvasJS.Chart("pomodoro_doughnut", {
+      animationEnabled: true,
+      backgroundColor: "transparent",
+      title: {
+        fontColor: "#848484",
+        fontSize: 60,
+        horizontalAlign: "center",
+        text: "19%",
+        fontFamily: "Roboto",
+        verticalAlign: "center"
+      },
+      toolTip: {
+        backgroundColor: "#ffffff",
+        borderThickness: 0,
+        cornerRadius: 0,
+        fontColor: "#424242"
+      },
+      data: [
+        {
+          explodeOnClick: false,
+          innerRadius: "96%",
+          radius: "90%",
+          startAngle: 270,
+          type: "doughnut",
+          dataPoints: [
+            { y: 19, color: "#c70000", toolTipContent: "Netherlands: $<span>" + CanvasJS.formatNumber(Math.round(19 / 100 * totalRevenue), '#,###,###') + "</span>" },
+            { y: 81, color: "#424242", toolTipContent: null }
+          ]
+        }
+      ]
+    });
+
+    // CanvasJS doughnut chart to show annual sales percentage from Germany(DE)
+    var unproductivity_doughnut = new CanvasJS.Chart("unproductivity_doughnut", {
+      animationEnabled: true,
+      backgroundColor: "transparent",
+      title: {
+        fontColor: "#848484",
+        fontSize: 60,
+        horizontalAlign: "center",
+        text: "12%",
+        fontFamily: "Roboto",
+        verticalAlign: "center"
+      },
+      toolTip: {
+        backgroundColor: "#ffffff",
+        borderThickness: 0,
+        cornerRadius: 0,
+        fontColor: "#424242"
+      },
+      data: [
+        {
+          explodeOnClick: false,
+          innerRadius: "96%",
+          radius: "90%",
+          startAngle: 270,
+          type: "doughnut",
+          dataPoints: [
+            { y: 12, color: "#c70000", toolTipContent: "Netherlands: $<span>" + CanvasJS.formatNumber(Math.round(12 / 100 * totalRevenue), '#,###,###') + "</span>" },
+            { y: 88, color: "#424242", toolTipContent: null }
+          ]
+        }
+      ]
+    });
+
+          jQuery.scrollSpeed(100, 400); // for smooth mouse wheel scrolling
+
+    // jQuery.inview plugin
+    $('.inview').one('inview', function (e, isInView) {
+      if (isInView) {
+        switch (this.id) {
+          case "steps_doughnut": steps_doughnut.render();
+            break;
+          case "pomodoro_doughnut": pomodoro_doughnut.render();
+            break;
+          case "unproductivity_doughnut": unproductivity_doughnut.render();
+            break;
+        }
+      }
+    });
+
+  });
 }
-
-function get_steps_doughnut(){
-  var max_value = 5000
-  chartOptions = {
-    segmentShowStroke: false,
-    percentageInnerCutout: 75,
-    animation: false
-  };
-
-  chartData = [{
-      value: 0,
-      color: '#4FD134'
-  },{
-      value: max_value,
-      color: '#DDDDDD'
-  }];
-  var ctx = $('#steps_doughnut').get(0).getContext("2d");
-  var steps_doughnut = new Chart(ctx).Doughnut(chartData,chartOptions);
-  return steps_doughnut
-}
-
 
 $(document).ready(function() {
   //Create unproductivity_doughnut
-
+  create_doughnuts();
   load_data();
 });
